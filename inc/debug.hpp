126075ecaa86015940d72a814eab8548f0a3e8b6
mike krinkin <krinkin.m.u@gmail.com>
2013-11-04 17:42:22 +0400

    fix missed namespace

--- inc/debug.hpp
+++ inc/debug.hpp
@@ -3,6 +3,9 @@
 
 #include <llvm/Support/raw_ostream.h>
 
+namespace pa
+{
+
 template <typename Stream>
 Stream & debug_output(Stream & out)
 { return out; }
@@ -14,8 +17,10 @@ Stream & debug_output(Stream & out, Type const & obj, Types const & ... args)
     return debug_output(out, args ...);
 }
 
-#define warn(...) debug_output(llvm::errs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
-#define info(...) debug_output(llvm::outs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
-#define debug(...) debug_output(llvm::outs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
+} /* namespace pa */
+
+#define warn(...) pa::debug_output(llvm::errs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
+#define info(...) pa::debug_output(llvm::outs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
+#define debug(...) pa::debug_output(llvm::outs(), "In ", __func__, ": ", __VA_ARGS__, "\n")
 
 #endif /*__INC_DEBUG_HPP__*/
